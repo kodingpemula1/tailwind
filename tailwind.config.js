5db4b088840203a8839e84af3ca1ca86e44e6973
kodingpemula1 <andrebkng@gmail.com>
2023-05-26 12:15:32 +0800

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', 
+  'DasarDasar-Tailwind-CSS/index.html',
+  './Tailwind-Css-Form-Button-dan-Table/index.html',
+  './Tailwind-CSS-Grid-dan-Flexbox/index.html',
+  './Tailwind-CSS-Grid-dan-Flexbox/Task2/index.html',
+  './Tailwind-CSS-Card/task1/index.html',
+  './Tailwind-CSS-Card/task2/index.html',
+  './Tailwind-Project/Page1/index.html',
+  './Tailwind-Project/Page2/index.html',
+  './Tailwind-Project/Page3/index.html',
+  './Tailwind-Project/Page4/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
